d63eaf97c26465da607c486078114c96c94718a5
Lloyd Lobo <lloydlobo4@gmail.com>
2023-11-20 09:04:19 +0530

    chore: setup basic application iteration json printing

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import json
+
 print("Hello from SmartCV")
+
+CV_NAME = "John Doe"
+CV_EMAIL = "johndoe@example.com"
+
+base_data = {
+    "name": CV_NAME,
+    "email": CV_EMAIL,
+}
+
+applications = []
+
+for i in range(10):
+    application = base_data.copy()
+    application['summary'] = "todo"
+    application['id'] = i
+    applications.append(application)
+
+print(json.dumps(applications, indent=2))
