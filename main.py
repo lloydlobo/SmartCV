2f91e43056490e7c6317e4b845a4f9b38d854a1e
Lloyd Lobo <lloydlobo4@gmail.com>
2023-11-20 16:02:40 +0530

    feat: update markup and add css style

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import os
 import sys
+import shutil
 
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-pdf_path = 'resume.pdf'
-name = "John Doe"
-output_pdf_file_path = f'{name.replace(' ', '_')}_resume.pdf'
+YAML_FILE_PATH = '.SmartCV.data.yml'
+template_dir, template_file = "templates", "resume_template.html"
+css_file = "style_resume_template.css"
+build_dir = "build"
 
 # ------------------------------------------------------------------------------
 
-YAML_FILE_PATH = '.SmartCV.data.yml'
 yaml_data: any
 with open(YAML_FILE_PATH, 'r') as base_data_file:
     yaml_data = yaml.safe_load(base_data_file)
@@ -40,7 +41,6 @@ def generate_resume(yaml_file, template_file, output_pdf):
 
     # --------------------------------------------------------------------------
     # Load Jinja2 template
-    template_dir, template_file = "templates", "resume_template.html"
     env = Environment(loader=FileSystemLoader(template_dir))
     template = env.get_template('resume_template.html')
 
@@ -59,7 +59,17 @@ def main():
     dir_root = os.path.dirname(os.path.dirname(__file__))
     template_file = os.path.join(dir_root, 'resume_template.html')
 
-    generate_resume('.SmartCV.data.yml', template_file, 'output_resume.pdf')
+    html_resume = generate_resume('.SmartCV.data.yml', template_file,
+                                  'output_resume.pdf')
+
+    # dir_build = os.path.join(dir_root, "build")
+    os.makedirs("build", exist_ok=True)
+    with open(os.path.join("build", "resume.html"), "w",
+              encoding="utf-8") as html_output_file:
+        print(html_resume)
+        html_output_file.write(html_resume)
+        shutil.copy(src=os.path.join(template_dir, css_file),
+                    dst=os.path.join(build_dir, css_file))
 
 
 if __name__ == "__main__":
