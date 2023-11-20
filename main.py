b205e52a1903e7cf7c535e9af226e24f749123f5
Lloyd Lobo <lloydlobo4@gmail.com>
2023-11-20 15:06:46 +0530

    feat: read yaml and parse out resume as html

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
-import json
+import os
+import sys
 
-print("Hello from SmartCV")
+import yaml
+from jinja2 import Environment, FileSystemLoader
 
-CV_NAME = "John Doe"
-CV_EMAIL = "johndoe@example.com"
+pdf_path = 'resume.pdf'
+name = "John Doe"
+output_pdf_file_path = f'{name.replace(' ', '_')}_resume.pdf'
 
-base_data = {
-    "name": CV_NAME,
-    "email": CV_EMAIL,
-}
+# ------------------------------------------------------------------------------
 
-applications = []
+YAML_FILE_PATH = '.SmartCV.data.yml'
+yaml_data: any
+with open(YAML_FILE_PATH, 'r') as base_data_file:
+    yaml_data = yaml.safe_load(base_data_file)
+yaml_output = yaml.dump(yaml_data, default_flow_style=False, sort_keys=False)
 
-for i in range(10):
-    application = base_data.copy()
-    application['summary'] = "todo"
-    application['id'] = i
-    applications.append(application)
 
-print(json.dumps(applications, indent=2))
+# ------------------------------------------------------------------------------
+
+class SmartCV:
+    def __init__(self, name):
+        self.name = name
+
+    def generate_resume(self):
+        resume = []
+        resume.append(f"Name: {self.name}")
+
+        return '\n'.join(resume)
+
+
+# ------------------------------------------------------------------------------
+
+def generate_resume(yaml_file, template_file, output_pdf):
+    # --------------------------------------------------------------------------
+    # Load YAML data
+    with open(yaml_file, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # --------------------------------------------------------------------------
+    # Load Jinja2 template
+    template_dir, template_file = "templates", "resume_template.html"
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template('resume_template.html')
+
+    # --------------------------------------------------------------------------
+    # Render the template with YAML data
+    rendered_resume = template.render(data=data)
+    print(rendered_resume)
+    return rendered_resume
+
+    # --------------------------------------------------------------------------
+    # Generate PDF using WeasyPrint
+    # HTML(string=rendered_resume).write_pdf(output_pdf)
+
+
+def main():
+    dir_root = os.path.dirname(os.path.dirname(__file__))
+    template_file = os.path.join(dir_root, 'resume_template.html')
+
+    generate_resume('.SmartCV.data.yml', template_file, 'output_resume.pdf')
+
+
+if __name__ == "__main__":
+    sys.exit(main())
