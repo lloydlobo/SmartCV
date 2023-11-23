30d979479ff88ce1cbd268680cdca842ba11805e
Restyled.io <commits@restyled.io>
2023-11-23 08:46:57 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import shutil
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-YAML_FILE_PATH = '.SmartCV.data.yml'
+YAML_FILE_PATH = ".SmartCV.data.yml"
 template_dir, template_file = "templates", "resume_template.html"
 css_file = "style_resume_template.css"
 build_dir = "build"
@@ -13,12 +13,13 @@ build_dir = "build"
 # ------------------------------------------------------------------------------
 
 yaml_data: any
-with open(YAML_FILE_PATH, 'r') as base_data_file:
+with open(YAML_FILE_PATH, "r") as base_data_file:
     yaml_data = yaml.safe_load(base_data_file)
 yaml_output = yaml.dump(yaml_data, default_flow_style=False, sort_keys=False)
 
 
 # ------------------------------------------------------------------------------
+
 
 class SmartCV:
     def __init__(self, name):
@@ -28,21 +29,22 @@ class SmartCV:
         resume = []
         resume.append(f"Name: {self.name}")
 
-        return '\n'.join(resume)
+        return "\n".join(resume)
 
 
 # ------------------------------------------------------------------------------
 
+
 def generate_resume(yaml_file, template_file, output_pdf):
     # --------------------------------------------------------------------------
     # Load YAML data
-    with open(yaml_file, 'r', encoding='utf-8') as file:
+    with open(yaml_file, "r", encoding="utf-8") as file:
         data = yaml.safe_load(file)
 
     # --------------------------------------------------------------------------
     # Load Jinja2 template
     env = Environment(loader=FileSystemLoader(template_dir))
-    template = env.get_template('resume_template.html')
+    template = env.get_template("resume_template.html")
 
     # --------------------------------------------------------------------------
     # Render the template with YAML data
@@ -56,19 +58,23 @@ def generate_resume(yaml_file, template_file, output_pdf):
 
 def main():
     dir_root = os.path.dirname(os.path.dirname(__file__))
-    template_file = os.path.join(dir_root, 'resume_template.html')
+    template_file = os.path.join(dir_root, "resume_template.html")
 
-    html_resume = generate_resume('.SmartCV.data.yml', template_file,
-                                  'output_resume.pdf')
+    html_resume = generate_resume(
+        ".SmartCV.data.yml", template_file, "output_resume.pdf"
+    )
 
     # dir_build = os.path.join(dir_root, "build")
     os.makedirs("build", exist_ok=True)
-    with open(os.path.join("build", "resume.html"), "w",
-              encoding="utf-8") as html_output_file:
+    with open(
+        os.path.join("build", "resume.html"), "w", encoding="utf-8"
+    ) as html_output_file:
         print(html_resume)
         html_output_file.write(html_resume)
-        shutil.copy(src=os.path.join(template_dir, css_file),
-                    dst=os.path.join(build_dir, css_file))
+        shutil.copy(
+            src=os.path.join(template_dir, css_file),
+            dst=os.path.join(build_dir, css_file),
+        )
 
 
 if __name__ == "__main__":
